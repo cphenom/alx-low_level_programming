46a306e42e62f68f81cda719ae95b0beea7f3016
chimezie <onwudiwechimezie@gmail.com>
2022-07-01 08:05:24 -0700

    C program to check for random positive or negavive number or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+  * main - starting point of code running
+  * checks if n is positive
+  * negative or zero
+  * Return: 0 is successful
+  */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+	/* checker for positive */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	/* checker for negative */
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	/* then it can only be zero */
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This is the directorybfor writing C codes for if else while and variables for alx task
