35578b90291c9d0dd2f3925fb202a8f13c1dbb47
chimezie <onwudiwechimezie@gmail.com>
2022-07-14 00:19:49 -0700

    function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory will contain task solutions to my alx more pointer, array and string task
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strcat(char *dest, char *src);
+
+#endif
+/**
+  * Author: Chimezie Onwudiwe
+  * main header file to contain our function prototypes
+  * exempting duplicates
+  */
