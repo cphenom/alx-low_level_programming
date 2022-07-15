de1181ec76f72d62faa2e15742a783eed848c9d9
chimezie <onwudiwechimezie@gmail.com>
2022-07-14 18:04:06 -0700

    a function that prints a buffer with certain conditions

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,7 +12,8 @@ char *leet(char *s);
 char *rot13(char *s);
 void print_number(int n);
 int _putchar(char);
- char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
 /**
