56074704b576430d857cf07b0e04f4b9b054f0af
chimezie <onwudiwechimezie@gmail.com>
2022-07-28 17:33:15 -0700

    fixed error for program that multiplies two positive numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <stdio.h>
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -10,7 +11,5 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int is_digit(char *s);
 int _putchar(char c);
-
-#include <stdio.h>
 
 #endif
