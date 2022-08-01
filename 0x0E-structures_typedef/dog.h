232e2485941ec2936450c4c9c2cd9203f0ee45a9
chimezie <onwudiwechimezie@gmail.com>
2022-08-01 16:39:00 -0700

    Define a new type struct dog with the following elements name, age, owner

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure definition about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Author: Chimezie Onwudiwe
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
